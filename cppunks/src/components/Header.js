647a8e81387eb5ff2bf7c7a1dfa84a1a2c5455ab
Simone Ballard <simone.a.ballard@gmail.com>
2022-04-12 16:20:25 -0500

    created react app and some set up

---
+++ cppunks/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+    <div className='logoContainer'>
+        <img src={punkLogo} className='punkLogo' alt='' />
+    </div>
+    </div>
+  )
+}
+
+export default Header
